661efa777f909500109fe334ba06cc87238980c3
Mario <mario@kraferd.com>
2016-07-08 12:07:54 -0700

    added comic book and artist models

---
+++ src/ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+    public class Artist
+    {
+        public string Name { get; set; }
+    }
+}
